7565ac5659337383a487834c892ea78ba8a0e9e8
anna-vasylenko <anna.bilych16@gmail.com>
2024-10-22 15:30:39 +0300

    add hooks

--- src/db/models/Contact.js
+++ src/db/models/Contact.js
@@ -1,6 +1,7 @@
 import { Schema, model } from 'mongoose';
 
 import { contactTypeList, phoneNumberRegExp} from '../../constants/contacts.js';
+import { handleSaveError, setUpdateSettings } from './hooks.js';
 
 const contactSchema = new Schema(
   {
@@ -29,6 +30,13 @@ const contactSchema = new Schema(
   },
   { versionKey: false, timestamps: true },
 );
+
+contactSchema.post("save", handleSaveError);
+
+contactSchema.pre("findOneAndUpdate", setUpdateSettings);
+
+contactSchema.post("findOneAndUpdate", handleSaveError);
+
 
 const ContactCollection = model('contact', contactSchema);
 
